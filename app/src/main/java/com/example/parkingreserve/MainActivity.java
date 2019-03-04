55f1085c3b09aa7d33c973b8445664e9231c2627
muradyan90 <muradyan3900@gmail.com>
2019-03-04 20:25:31 +0400

    third commit

--- app/src/main/java/com/example/parkingreserve/MainActivity.java
+++ app/src/main/java/com/example/parkingreserve/MainActivity.java
@@ -10,11 +10,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
+import in.galaxyofandroid.spinerdialog.SpinnerDialog;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     public static final String AM_PHONE_CODE = "+374";
     public static final String PHONE_INTENT_KEY="phone number";
@@ -23,32 +34,63 @@ public class MainActivity extends AppCompatActivity {
     Button nextBtn;
 
 
+
+    SpinnerDialog spinnerModel,spinnerColor;
+    TextView selectCarModel;
+    TextView selectCarColor;
+
+    TextView modelHide,colorHide,numberHide;
+     EditText plateNumber;
+
+
+     String sModel,sColor,sPlate,sNum;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+            initViews();
+        initModelPicker();
+        initColorPicker();
 
-        numberInput=findViewById(R.id.sign_in_number_input);
-        nextBtn=findViewById(R.id.sign_in_next_btn);
+
+
+        selectCarColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                      spinnerColor.showSpinerDialog();
+            }
+        });
+
+
+
+        selectCarModel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinnerModel.showSpinerDialog();
+            }
+        });
+
+
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String number=numberInput.getText().toString();
-                if (number.isEmpty() || number.length()<8){
-                    numberInput.setError("Valid number is required");
-                    numberInput.requestFocus();
-                    return;
+                sNum=numberInput.getText().toString();
+                sPlate=plateNumber.getText().toString();
+
+                if (isChecked()) {
+
+
+                    String phoneNumber = AM_PHONE_CODE.concat(sNum);
+
+                    Intent intent = new Intent(MainActivity.this, VerifyPhoneActivity.class);
+                    intent.putExtra(PHONE_INTENT_KEY, phoneNumber);
+                    startActivity(intent);
                 }
-
-                String phoneNumber=AM_PHONE_CODE.concat(number);
-
-                Intent intent=new Intent(MainActivity.this,VerifyPhoneActivity.class);
-                intent.putExtra(PHONE_INTENT_KEY,phoneNumber);
-                startActivity(intent);
-
             }
         });
 
@@ -66,4 +108,120 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    public void initModelPicker(){
+
+
+        ArrayList<String> mCarModels=new ArrayList<String>(Arrays.asList(Utils.carModelList));
+
+        spinnerModel =new SpinnerDialog(MainActivity.this,mCarModels,Utils.CAR_MODEL_DIALOG_TEXT);
+        spinnerModel.bindOnSpinerListener(new OnSpinerItemClick() {
+            @Override
+            public void onClick(String item, int position) {
+
+                sModel=item;
+                selectCarModel.setText(item);
+                modelHide.setVisibility(View.VISIBLE);
+                selectCarModel.setError(null);
+            }
+        });
+    }
+
+
+public void initColorPicker(){
+
+
+final ArrayList<Integer> colors =new ArrayList<>(Utils.getCarColorList(this).keySet());
+
+    spinnerColor =new SpinnerDialog(MainActivity.this,new ArrayList<String>(Utils.getCarColorList(this).values()),Utils.CAR_COLOR_DIALOG_TEXT);
+    spinnerColor.bindOnSpinerListener(new OnSpinerItemClick() {
+        @Override
+        public void onClick(String item, int position) {
+
+
+            sColor=item;
+          selectCarColor.setText(item);
+          selectCarColor.setTextColor(colors.get(position));
+          colorHide.setVisibility(View.VISIBLE);
+
+          selectCarColor.setError(null);
+
+
+
+        }
+    });
+
+
+}
+
+
+
+
+public void initViews(){
+
+    selectCarModel=findViewById(R.id.car_model_select);
+    selectCarColor=findViewById(R.id.car_color_select);
+    nextBtn=findViewById(R.id.sign_in_next_btn);
+    numberInput=findViewById(R.id.sign_in_number_input);
+    plateNumber=findViewById(R.id.car_plate_input);
+
+
+
+     numberHide=findViewById(R.id.number_hide);
+     colorHide=findViewById(R.id.color_hide);
+     modelHide=findViewById(R.id.model_hide);
+
+}
+
+
+public boolean isChecked(){
+
+        int counter=0;
+
+    if (sNum==null || sNum.length()<8){
+        numberInput.setError("valid number is required");
+        numberInput.requestFocus();
+
+        counter++;
+
+    }
+   if (sModel==null){
+
+        selectCarModel.setError("car model is required");
+        selectCarModel.requestFocus();
+
+        counter++;
+
+    }
+    if (sColor==null){
+
+        selectCarColor.setError("car color is required");
+        selectCarColor.requestFocus();
+
+        counter++;
+
+    }
+    if (sPlate.length()<7){
+       plateNumber.setError("valid number is required");
+       plateNumber.requestFocus();
+        counter++;
+    }
+
+
+if (counter>0){
+    return false;
+}
+else return true;
+
+}
+
+
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/parkingreserve/Utils.java
@@ -0,0 +1,298 @@
+package com.example.parkingreserve;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utils {
+
+
+    public static final String carModelList[]={"Автокам",
+            "Бронто",
+            "ГАЗ",
+            "Ё-мобиль",
+            "ЗАЗ",
+            "ЗИЛ",
+            "ЗиС",
+            "ИЖ",
+            "Канонир",
+            "Комбат",
+            "ЛуАЗ",
+            "Москвич",
+            "СМ",
+            "ТагАЗ",
+            "УАЗ",
+            "Эксклюзив",
+            "AC",
+            "Acura",
+            "Adler",
+            "Alfa Romeo",
+            "Alpina",
+            "Alpine",
+            "AM General",
+            "AMC",
+            "Ariel",
+            "Aro",
+            "Asia",
+            "Aston Martin",
+            "Audi",
+            "Austin",
+            "Autobianchi",
+            "Bajaj",
+            "Baltijas Dzips",
+            "Batmobile",
+            "Beijing",
+            "Bentley",
+            "Bertone",
+            "Bilenkin",
+            "Bitter",
+            "BMW",
+            "Borgward",
+            "Brabus",
+            "Bricklin",
+            "Brilliance",
+            "Bristol",
+            "Bufori",
+            "Bugatti",
+            "Buick",
+            "BYD",
+            "Byvin",
+            "Cadillac",
+            "Callaway",
+            "Carbodies",
+            "Caterham",
+            "Changan",
+            "ChangFeng",
+            "Chery",
+            "Chevrolet",
+            "Chrysler",
+            "Citroen",
+            "Cizeta",
+            "Coggiola",
+            "Dacia",
+            "Dadi",
+            "Daewoo2",
+            "Daihatsu",
+            "Daimler",
+            "Datsun",
+            "De Tomaso",
+            "Delage",
+            "DeLorean",
+            "Derways",
+            "DeSoto",
+            "Dodge",
+            "DongFeng",
+            "Doninvest",
+            "Donkervoort",
+            "DS",
+            "E-Car",
+            "Eagle",
+            "Eagle Cars",
+            "Ecomotors",
+            "Excalibur",
+            "FAW",
+            "Ferrari",
+            "Fiat",
+            "Fisker",
+            "Ford",
+            "Foton",
+            "FSO",
+            "Fuqi",
+            "Geely",
+            "Genesis",
+            "Geo",
+            "GMC",
+            "Gonow",
+            "Gordon",
+            "Great Wall",
+            "Hafei",
+            "Haima",
+            "Hanomag",
+            "Haval",
+            "Hawtai",
+            "Hindustan",
+            "Hispano-Suiza",
+            "Holden",
+            "Honda",
+            "HuangHai",
+            "Hudson",
+            "Hummer",
+            "Hyundai",
+            "Infiniti",
+            "Innocenti",
+            "Invicta",
+            "Iran Khodro",
+            "Isdera",
+            "Isuzu",
+            "JAC",
+            "Jaguar",
+            "Jeep",
+            "Jensen",
+            "JMC",
+            "Kia",
+            "Koenigsegg",
+            "KTM AG",
+            "LADA (ВАЗ)",
+            "Lamborghini",
+            "Lancia",
+            "Land Rover",
+            "Landwind",
+            "Lexus",
+            "Liebao Motor",
+            "Lifan",
+            "Lincoln",
+            "Lotus",
+            "LTI",
+            "Luxgen",
+            "Mahindra",
+            "Marcos",
+            "Marlin",
+            "Marussia",
+            "Maruti",
+            "Maserati",
+            "Maybach",
+            "Mazda",
+            "McLare1",
+            "Mega",
+            "Mercedes-Benz",
+            "Mercury",
+            "Metrocab",
+            "MG",
+            "Microcar",
+            "Minelli",
+            "MINI",
+            "Mitsubishi",
+            "Mitsuoka",
+            "Morgan",
+            "Morris",
+            "Nissan",
+            "Noble",
+            "Oldsmobile",
+            "Opel",
+            "Osca",
+            "Packard",
+            "Pagani",
+            "Panoz",
+            "Perodua",
+            "Peugeot",
+            "PGO",
+            "Piaggio",
+            "Plymouth",
+            "Pontiac",
+            "Porsche",
+            "Premier",
+            "Proton",
+            "PUCH",
+            "Puma",
+            "Qoros",
+            "Qvale",
+            "Ravon",
+            "Reliant",
+            "Renaissance",
+            "Renault",
+            "Renault Samsung",
+            "Rezvani",
+            "Rimac",
+            "Rolls-Royce",
+            "Ronart",
+            "Rover",
+            "Saab",
+            "Saleen",
+            "Santana",
+            "Saturn",
+            "Scion",
+            "SEAT",
+            "Shanghai Maple",
+            "ShuangHuan",
+            "Skoda",
+            "Smart",
+            "Soueast",
+            "Spectre",
+            "Spyker",
+            "SsangYong",
+            "Steyr",
+            "Subaru",
+            "Suzuki",
+            "Talbot",
+            "TATA",
+            "Tatra",
+            "Tazzari",
+            "Tesla",
+            "Think",
+            "Tianma",
+            "Tianye",
+            "Tofas",
+            "Toyota",
+            "Trabant",
+            "Tramontana",
+            "Triumph",
+            "TVR",
+            "Ultima",
+            "Vauxhall",
+            "Vector",
+            "Venturi",
+            "Volkswagen",
+            "Volvo",
+            "Vortex",
+            "W Motors",
+            "Wanderer",
+            "Wartburg",
+            "Westfield",
+            "Wiesmann",
+            "Willys",
+            "Xin Kai",
+            "Zastava",
+            "Zenos",
+            "Zenvo",
+            "Zibar",
+            "Zotye",
+            "ZX"};
+
+
+    public static final HashMap<Integer,String> getCarColorList(Context context){
+
+
+        HashMap <Integer,String> carColorlist=new HashMap<>();
+
+
+
+        carColorlist.put(context.getResources().getColor(R.color.Black),"Black");
+        carColorlist.put(context.getResources().getColor(R.color.Champagne),"Champagne");
+        carColorlist.put(context.getResources().getColor(R.color.White),"White");
+        carColorlist.put(context.getResources().getColor(R.color.Blue),"Blue");
+        carColorlist.put(context.getResources().getColor(R.color.DarkBlue),"Dark Blue");
+        carColorlist.put(context.getResources().getColor(R.color.Violet),"Violet");
+        carColorlist.put(context.getResources().getColor(R.color.Yellow),"Yellow");
+        carColorlist.put(context.getResources().getColor(R.color.Green),"Green");
+        carColorlist.put(context.getResources().getColor(R.color.Grey),"Grey");
+        carColorlist.put(context.getResources().getColor(R.color.Pink),"Pink");
+
+        return carColorlist;
+    }
+
+
+
+
+
+    public static final String CAR_MODEL_DIALOG_TEXT ="Select Your Car Model";
+
+    public static final String CAR_COLOR_DIALOG_TEXT="Select Your Car Color";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
